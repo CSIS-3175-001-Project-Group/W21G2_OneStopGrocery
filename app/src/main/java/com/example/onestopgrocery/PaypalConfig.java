bc073ccb716c0462b7da47c2c73293267e933270
relovag <relovag@student.douglascollege.ca>
2021-03-19 13:36:54 -0700

    Add Paypal integration

---
+++ app/src/main/java/com/example/onestopgrocery/PaypalConfig.java
@@ -0,0 +1,7 @@
+package com.example.onestopgrocery;
+
+public class PaypalConfig {
+
+    public static final String PAYPAL_CLIENT_ID = "ATPH0F2wZMnXnvlr_Zdb3Zr249c6DTvbDjD7e2SWTEh-dsrJNuulD7hQ5cFegs6Dv1GLG1jEW04yB3ah";
+
+}
