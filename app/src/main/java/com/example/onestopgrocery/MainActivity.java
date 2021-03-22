b0b20e4de076b4afebc134017bdc5027b28f9ff3
anick007 <68749413+anick007@users.noreply.github.com>
2021-03-21 20:00:14 -0700

    Credentials added

--- app/src/main/java/com/example/onestopgrocery/MainActivity.java
+++ app/src/main/java/com/example/onestopgrocery/MainActivity.java
@@ -19,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void run() {
                 startActivity(new Intent(MainActivity.this,
-                        HomeActivity.class));
+                        SignUpActivity.class));
                 finish();
             }
         };
