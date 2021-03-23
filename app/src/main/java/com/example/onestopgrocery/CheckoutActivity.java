3c7e03e07325ecc18242b6364be68a902cc2f4fd
relovag <relovag@student.douglascollege.ca>
2021-03-23 15:55:03 -0700

    Simplify checkout activity

--- app/src/main/java/com/example/onestopgrocery/CheckoutActivity.java
+++ app/src/main/java/com/example/onestopgrocery/CheckoutActivity.java
@@ -5,26 +5,35 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.example.onestopgrocery.entities.Payment;
+import com.paypal.android.sdk.payments.PayPalAuthorization;
 import com.paypal.android.sdk.payments.PayPalConfiguration;
+import com.paypal.android.sdk.payments.PayPalFuturePaymentActivity;
 import com.paypal.android.sdk.payments.PayPalPayment;
 import com.paypal.android.sdk.payments.PayPalService;
 import com.paypal.android.sdk.payments.PaymentActivity;
+import com.paypal.android.sdk.payments.PaymentConfirmation;
+
+import org.json.JSONException;
 
 import java.math.BigDecimal;
 
 public class CheckoutActivity extends AppCompatActivity {
 
     private Button confirmPayBtn;
-    private int PAYPAL_REQ_CODE = 12;
-    private static PayPalConfiguration paypalConfig = new PayPalConfiguration()
-            .environment(PayPalConfiguration.ENVIRONMENT_SANDBOX)
-            .clientId(PaypalConfig.PAYPAL_CLIENT_ID);
-
+    public static final String PAYPAL_CLIENT_ID = "ATPH0F2wZMnXnvlr_Zdb3Zr249c6DTvbDjD7e2SWTEh-dsrJNuulD7hQ5cFegs6Dv1GLG1jEW04yB3ah";
+    private static final int REQ_CODE_PAYMENT = 1;
+    private static final int REQ_CODE_FUTURE_PAYMENT = 2;
+    private static final String CONFIG_ENV = PayPalConfiguration.ENVIRONMENT_NO_NETWORK;
+    private static PayPalConfiguration payPalConfig;
+    PayPalPayment cartPayment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,29 +42,38 @@ public class CheckoutActivity extends AppCompatActivity {
 
         confirmPayBtn = findViewById(R.id.confirmPayBtn);
 
-        Intent intent = new Intent(this, PayPalService.class);
-        intent.putExtra(PayPalService.EXTRA_PAYPAL_CONFIGURATION, paypalConfig);
-        startService(intent);
-
-
         confirmPayBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
                 payWithPayPal();
             }
         });
+        configPayPal();
+    }
+
+    private void configPayPal() {
+        payPalConfig = new PayPalConfiguration()
+        .environment(CONFIG_ENV)
+        .clientId(PAYPAL_CLIENT_ID)
+        .merchantName("PayPal Login")
+        .merchantPrivacyPolicyUri(Uri.parse("https://www.example.com/privacy"))
+        .merchantUserAgreementUri(Uri.parse("https:www/example.com/legal"));
+
     }
 
     private void payWithPayPal() {
 
-        PayPalPayment payment = new PayPalPayment(new BigDecimal(100), "CAD",
-                "Complete Your Payment", PayPalPayment.PAYMENT_INTENT_SALE);
+        Intent intent = new Intent(this, PayPalService.class);
+        intent.putExtra(PayPalService.EXTRA_PAYPAL_CONFIGURATION, payPalConfig);
+        startService(intent);
 
-        Intent payIntent = new Intent(this, PaymentActivity.class);
-        payIntent.putExtra(PayPalService.EXTRA_PAYPAL_CONFIGURATION, paypalConfig);
-        payIntent.putExtra(PaymentActivity.EXTRA_PAYMENT, payment);
-
-        startActivityForResult(payIntent, PAYPAL_REQ_CODE);
+        cartPayment = new PayPalPayment(new BigDecimal(String.valueOf("9.99")), "CAD",
+                "Complete Payment", PayPalPayment.PAYMENT_INTENT_SALE);
+        Intent paymentIntent = new Intent(this, PaymentActivity.class);
+        paymentIntent.putExtra(PaymentActivity.EXTRA_PAYMENT, cartPayment);
+        paymentIntent.putExtra(PayPalService.EXTRA_PAYPAL_CONFIGURATION, payPalConfig);
+        startActivityForResult(paymentIntent, REQ_CODE_PAYMENT);
 
     };
 
@@ -63,12 +81,35 @@ public class CheckoutActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        if (requestCode == PAYPAL_REQ_CODE) {
-            System.out.println("Request code reached");
+        if (requestCode == REQ_CODE_PAYMENT) {
             if (resultCode == Activity.RESULT_OK) {
-                Toast.makeText(this, "Payment completed successfully", Toast.LENGTH_SHORT).show();
-            } else {
-                Toast.makeText(this, "Payment was unsuccessful", Toast.LENGTH_SHORT).show();
+                PaymentConfirmation confirm = data
+                        .getParcelableExtra(PaymentActivity.EXTRA_RESULT_CONFIRMATION);
+                if (confirm != null) {
+                    try {
+                        System.out.println(confirm.toJSONObject()
+                                .toString(4));
+                    } catch (JSONException e) {
+                        Toast.makeText(this, e.toString(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+            } else if (resultCode == Activity.RESULT_CANCELED) {
+                Toast.makeText(this, "Payment has been cancelled", Toast.LENGTH_SHORT).show();
+            } else if (resultCode == PaymentActivity.RESULT_EXTRAS_INVALID) {
+                Toast.makeText(this, "An error occurred while processing payment", Toast.LENGTH_SHORT).show();
+            }
+        } else if (requestCode == REQ_CODE_FUTURE_PAYMENT) {
+            if (resultCode == Activity.RESULT_OK) {
+                PayPalAuthorization authorization = data.getParcelableExtra(PayPalFuturePaymentActivity.EXTRA_RESULT_AUTHORIZATION);
+                if (authorization != null) {
+                    try {
+                        Log.i("Future Payment", authorization.toJSONObject().toString(4));
+                        String authorization_code = authorization.getAuthorizationCode();
+                        Log.d("Future Payment", authorization_code);
+                    } catch (Exception e) {
+                        Log.d("Future Payment", e.toString());
+                    }
+                }
             }
         }
     }
--- app/src/main/java/com/example/onestopgrocery/PaypalConfig.java
+++
@@ -1,7 +0,0 @@
-package com.example.onestopgrocery;
-
-public class PaypalConfig {
-
-    public static final String PAYPAL_CLIENT_ID = "ATPH0F2wZMnXnvlr_Zdb3Zr249c6DTvbDjD7e2SWTEh-dsrJNuulD7hQ5cFegs6Dv1GLG1jEW04yB3ah";
-
-}
