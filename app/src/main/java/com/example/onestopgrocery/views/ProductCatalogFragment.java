06e5e437c1e02f80ebea78d806ea0d7ca2c1720a
relovag <relovag@student.douglascollege.ca>
2021-03-24 18:56:31 -0700

    Create adapter director and remove old adapter and product activity

--- app/src/main/java/com/example/onestopgrocery/views/ProductCatalogFragment.java
+++ app/src/main/java/com/example/onestopgrocery/views/ProductCatalogFragment.java
@@ -7,8 +7,12 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 import androidx.recyclerview.widget.DividerItemDecoration;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,15 +23,17 @@ import com.example.onestopgrocery.databinding.FragmentProductBinding;
 import com.example.onestopgrocery.entities.Product;
 import com.example.onestopgrocery.viewmodels.OneStopViewModel;
 
-import java.util.List;
+
 
 
 public class ProductCatalogFragment extends Fragment
         implements GroceryProductAdapter.GroceryProductInterface {
 
+    private static final String TAG = "ProductCatalogFragment";
     FragmentProductBinding fragmentProductBinding;
     private GroceryProductAdapter groceryProductAdapter;
     private OneStopViewModel oneStopViewModel;
+    private NavController navController;
 
     public ProductCatalogFragment() {
         // Required empty public constructor
@@ -46,7 +52,7 @@ public class ProductCatalogFragment extends Fragment
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        groceryProductAdapter = new GroceryProductAdapter();
+        groceryProductAdapter = new GroceryProductAdapter(this);
         fragmentProductBinding.productRecyclerView.setAdapter(groceryProductAdapter);
         fragmentProductBinding.productRecyclerView.addItemDecoration(
                 new DividerItemDecoration(requireContext(), DividerItemDecoration.VERTICAL));
@@ -56,6 +62,8 @@ public class ProductCatalogFragment extends Fragment
         oneStopViewModel = new ViewModelProvider(requireActivity()).get(OneStopViewModel.class);
         oneStopViewModel.getProducts().observe(getViewLifecycleOwner(),
                 products -> groceryProductAdapter.submitList(products));
+
+        navController = Navigation.findNavController(view);
     }
 
     @Override
@@ -65,6 +73,8 @@ public class ProductCatalogFragment extends Fragment
 
     @Override
     public void onItemClick(Product product) {
-
+        Log.d(TAG, "Item clicked: " + product.toString());
+        oneStopViewModel.setProduct(product);
+        navController.navigate(R.id.action_productCatalogFragment_to_productDetailFragment);
     }
 }
