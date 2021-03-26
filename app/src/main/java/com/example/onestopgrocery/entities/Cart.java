76141fc0edaaef10016689a5d42593f155adc407
relovag <relovag@student.douglascollege.ca>
2021-03-26 07:50:02 -0700

    Update cart layout and create cart row layout

--- app/src/main/java/com/example/onestopgrocery/entities/Cart.java
+++ app/src/main/java/com/example/onestopgrocery/entities/Cart.java
@@ -8,8 +8,9 @@ import androidx.room.Index;
 import androidx.room.PrimaryKey;
 
 import java.util.Date;
+import java.util.Objects;
 
-@Entity(indices = {@Index(value = { "id" })}, tableName = "carts")
+@Entity(indices = {@Index(value = { "id" })}, tableName = "cart")
 public class Cart {
     @NonNull
     @PrimaryKey(autoGenerate = true)
@@ -19,8 +20,8 @@ public class Cart {
     @ColumnInfo(name = "user_id")
     public Long userId;
     @NonNull
-    @ColumnInfo(name = "product_id")
-    public Long product_id;
+    @ColumnInfo(name = "product")
+    public Product product;
     @NonNull
     @ColumnInfo(name = "quantity")
     public Integer quantity;
@@ -28,12 +29,49 @@ public class Cart {
     @ColumnInfo(name = "created_datetime")
     public Date createdDatetime;
 
-
-    public Cart(@NonNull Long userId, @NonNull Long product_id,
+    public Cart(@NonNull Long id, @NonNull Long userId, @NonNull Product product,
                 @NonNull Integer quantity, @NonNull Date createdDatetime) {
+        this.id = id;
         this.userId = userId;
-        this.product_id = product_id;
+        this.product = product;
         this.quantity = quantity;
         this.createdDatetime = createdDatetime;
+    }
+
+    @NonNull
+    public Long getId() {
+        return id;
+    }
+
+    @NonNull
+    public Long getUserId() {
+        return userId;
+    }
+
+    @NonNull
+    public Product getProduct() {
+        return product;
+    }
+
+    @NonNull
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    @NonNull
+    public Date getCreatedDatetime() {
+        return createdDatetime;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Cart cart = (Cart) o;
+        return getId().equals(cart.getId()) &&
+                getUserId().equals(cart.getUserId()) &&
+                getProduct().equals(cart.getProduct()) &&
+                getQuantity().equals(cart.getQuantity()) &&
+                getCreatedDatetime().equals(cart.getCreatedDatetime());
     }
 }
